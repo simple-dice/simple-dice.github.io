a7c639a0840e5fbee4d9a751f29f0b4a60c89934
simple-dice <playsimpledice@gmail.com>
2016-02-14 19:47:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 950,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
   app_name: 'Simple Dice',
   // - For your faucet to work, you must register your site at Recaptcha
